e3219739bd0d25127120b39aa7b5bd0f27ba587b
har-wradim <jaera@yandex.com>
2018-01-07 22:03:02 +0200

    biotags.pl: exception handling, tag lists on demand, case sensitivity

--- perl/biotags.pl
+++ perl/biotags.pl
@@ -3,93 +3,20 @@
 use strict;
 use warnings;
 use Bio::SeqIO;
-
 use Getopt::Std;
 
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
-my $version = "biotags.pl v. 1.1\n";
+my $version = "biotags.pl v. 1.2\n";
 my $help    = "Use as:
   biotags.pl -i {input} [-p {primary}] -t {tags} [-H] [-d {delimiter}] > stdout
 
  -i - input file to parse
  -p - comma-separated list of primary tags to consider
- -T - comma-separated list of sequence tags:
-
-      accession (=accession_number)
-      accession_number
-      alphabet (dna, rna or protein)
-      annotation (defaults to comment)
-          comment
-          reference
-          # etc.
-      authority
-      can_call_new (useless)
-      definition (=desc)
-      desc
-      description
-      display_id
-      feature_count
-      get_num_of_annotations
-      id
-      is_circular
-      keywords
-      length
-      locus (=display_id)
-      namespace
-      object_id
-      organism (=species)
-      primary_id
-      revcom
-      seq
-      source (=species)
-      species (defaults to binomial)
-          binomial
-          classification
-          common_name
-          division
-          genus
-          ncbi_taxid
-          organelle
-          species
-          sub_species
-          taxon (not implemented)
-          tree (not implemented)
-          variant
-      subseq (throws exception)
-      translate
-      trunc (throws exception)
-      version
-
- -t - comma-separated list of feature tags. Any named tag can be used, as well as:
-
-      attach_seq (throws exception, useless)
-      contains (throws exception, useless)
-      display_name
-      end
-      entire_seq
-      equals (throws exception)
-      get_all_tags
-      get_tagset_values
-      get_tag_values
-      gff_string
-      has_tag (useless)
-      intersection (throws exception, useless)
-      location (formatted)
-      overlaps (throws exception, useless)
-      phase (throws exception)
-      primary_id (throws exception)
-      primary_tag
-      seq
-      seq_id
-      source_tag
-      spliced_seq
-      start
-      _static_gff_formatter (not implemented)
-      strand
-      union (throws exception, useless)
-
  -H - whether to include header in the output
  -d - field delimiter (tab is the default)
+ -T - comma-separated list of sequence tags (call -T? to show the list)
+ -t - comma-separated list of feature tags. Any named tag can be used
+      (call -t? to show the list of predefined tags)
 
 from https://github.com/har-wradim/miscngs/
 ";
@@ -98,6 +25,77 @@ our($opt_v, $opt_h, $opt_i, $opt_T, $opt_t, $opt_p, $opt_H, $opt_d);
 
 die $help    if not getopts('hvHi:T:t:p:d:') or $opt_h;
 die $version if $opt_v;
+if (defined $opt_T and $opt_T eq '?') {
+	print "accession (=accession_number)
+accession_number
+alphabet (dna, rna or protein)
+annotation (defaults to comment)
+    comment
+    date_changed
+    keyword
+    reference
+    # etc.
+authority
+can_call_new (useless)
+definition (=desc)
+desc
+description
+display_id
+feature_count
+get_num_of_annotations
+id
+is_circular
+keywords
+length
+locus (=display_id)
+namespace
+object_id
+organism (=species)
+primary_id
+revcom
+seq
+source (=species)
+species (defaults to binomial)
+    binomial
+    classification
+    common_name
+    division
+    genus
+    ncbi_taxid
+    organelle
+    species
+    sub_species
+    taxon (not implemented)
+    tree (not implemented)
+    variant
+translate
+version
+";
+	exit;
+}
+
+	if (defined $opt_t and $opt_t eq '?') {
+		print "display_name
+end
+entire_seq
+get_all_tags
+get_tagset_values
+get_tag_values
+gff_string
+location (formatted)
+phase
+primary_id
+primary_tag
+seq
+seq_id
+source_tag
+spliced_seq
+start
+_static_gff_formatter (not implemented)
+strand
+";
+	exit;
+}
 
 sub VERSION_MESSAGE {
 	print { shift } $version;
@@ -120,8 +118,8 @@ if (defined $opt_p) {
 my @stags = ();
 my @ftags = ();
 
-@stags = split /,/, lc $opt_T if $opt_T;
-@ftags = split /,/, lc $opt_t if $opt_t;
+@stags = split /,/, $opt_T if $opt_T;
+@ftags = split /,/, $opt_t if $opt_t;
 
 # print the header if requested
 printf "%s\n", join $opt_d, (@stags, @ftags) if defined $opt_H;
@@ -155,9 +153,13 @@ sub get_tag_values {
 		return join ';', $feature->get_tag_values($tag);
 	}
 
+	return '' if $tag =~ /^(new|can)$/;
+
 	# then object members
 	if ($feature->can($tag)) {
-		my @vals = $feature->$tag;
+		my @vals;
+		eval { @vals = $feature->$tag };
+		return '' if not defined $vals[0];
 		my $val = $vals[0];
 		my $ref = ref($val);
 		return $val->seq if $ref eq 'Bio::PrimarySeq';
@@ -172,9 +174,9 @@ sub get_seq_values {
 	my $seq = shift;
 	my $tag = shift;
 
-	return '' if $tag eq 'new' or $tag =~ /validate/;
+	return '' if $tag =~ /^(new|can)$/;
 
-	my %replace = (
+	my %aliases = (
 		locus      => 'display_id',
 		definition => 'desc',
 		accession  => 'accession_number',
@@ -182,14 +184,17 @@ sub get_seq_values {
 		source     => 'species'
 	);
 
-	$tag = $replace{$tag} if defined $replace{$tag};
+	$tag = $aliases{$tag} if defined $aliases{$tag};
 	if ($seq->can($tag)) {
-		my @vals = $seq->$tag;
+		my @vals;
+		eval { @vals = $seq->$tag };
+		return '' if not defined $vals[0];
 		my $val  = $vals[0];
 		return '' if not defined $val;
 		my $ref  = ref($val);
-		return $val->seq      if $ref eq 'Bio::Seq::RichSeq';
-		return $val->binomial if $ref eq 'Bio::Species';
+		return $val->seq                     if $ref eq 'Bio::Seq::RichSeq';
+		return $val->binomial                if $ref eq 'Bio::Species';
+		return $val->get_all_annotation_keys if $ref eq 'Bio::Annotation::Collection';
 		return join ";", @vals;
 	}
 	my @ac = $seq->get_Annotations($tag);
@@ -200,7 +205,9 @@ sub get_seq_values {
 	}
 	my $species = $seq->species;
 	if ($species->can($tag)) {
-		my @vals = $species->$tag;
+		my @vals;
+		eval { @vals = $species->$tag };
+		return '' if not defined $vals[0];
 		return join ';', @vals if defined $vals[0];
 	}
 	return '';
