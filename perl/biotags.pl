15d62936b70e4a54a7c285ca08b42ac2117e21eb
har-wradim <jaera@yandex.com>
2018-01-07 13:23:44 +0200

    Add sequence features

--- perl/biotags.pl
+++ perl/biotags.pl
@@ -7,24 +7,26 @@ use Bio::SeqIO;
 use Getopt::Std;
 
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
-my $version = "biotags.pl v. 0.1\n";
+my $version = "biotags.pl v. 1.0\n";
 my $help    = "Use as:
   biotags.pl -i {input} [-p {primary}] -t {tags} [-H] [-d {delimiter}] > stdout
 
  -i - input file to parse
  -p - comma-separated list of primary tags to consider
- -t - comma-separated list of tags to output (includes all explicitely named tags,
-      as well as 'start', 'strand', 'spliced_seq' etc. and reserved tags 'translate'
-      and 'join')
+ -T - comma-separated list of sequence tags (includes 'primary_id', 'desc' etc.,
+      as well as 'translate', 'revcom' etc.)
+ -t - comma-separated list of feature tags (includes all explicitely named tags,
+      as well as 'start', 'strand', 'spliced_seq', 'get_all_tags' etc. and
+      a reserved tag 'join')
  -H - whether to include header in the output
  -d - field delimiter (tab is the default)
 
 from https://github.com/har-wradim/miscngs/
 ";
 
-our($opt_v, $opt_h, $opt_i, $opt_t, $opt_p, $opt_H, $opt_d);
+our($opt_v, $opt_h, $opt_i, $opt_T, $opt_t, $opt_p, $opt_H, $opt_d);
 
-die $help    if not getopts('hvHi:t:p:d:') or $opt_h;
+die $help    if not getopts('hvHi:T:t:p:d:') or $opt_h;
 die $version if $opt_v;
 
 sub VERSION_MESSAGE {
@@ -37,27 +39,39 @@ sub HELP_MESSAGE {
 
 # check input args
 die "No input file specified\n" if not    $opt_i;
-die "Input file not found\n"    if not -s $opt_i;
-die "No tags specified\n" if not $opt_t;
+print STDERR "No tags specified\n" if not $opt_t and not $opt_T;
 
 $opt_d = "\t" if not defined $opt_d or $opt_d eq '\t';
 my %primaries;
 if (defined $opt_p) {
 	$primaries{$_} = 1 for split /,/, $opt_p;
 }
-my @tags = split /,/, $opt_t;
+
+my @stags = ();
+my @ftags = ();
+
+@stags = split /,/, lc $opt_T if $opt_T;
+@ftags = split /,/, lc $opt_t if $opt_t;
 
 # print the header if requested
-printf "%s\n", join $opt_d, @tags if defined $opt_H;
+printf "%s\n", join $opt_d, (@stags, @ftags) if defined $opt_H;
 
 # open the input and iterate over individual seqs/features
 my $in = Bio::SeqIO->new(-file => $opt_i) or die "$!\n";
-for my $seq ($in->next_seq) {
-	for my $feature ($seq->get_SeqFeatures) {
-		next if $opt_p and not $primaries{$feature->primary_tag};
+while (my $seq = $in->next_seq) {
+	if ($opt_t) {
+		for my $feature ($seq->get_SeqFeatures) {
+			next if $opt_p and not $primaries{$feature->primary_tag};
+			my @vals = ();
+			push @vals, get_seq_values($seq, $_)     for @stags;
+			push @vals, get_tag_values($feature, $_) for @ftags;
+			printf "%s\n", join $opt_d, @vals if @vals;
+		}
+	}
+	else {
 		my @vals = ();
-		push @vals, get_tag_values($feature, $_) for @tags;
-		printf "%s\n", join $opt_d, @vals;
+		push @vals, get_seq_values($seq, $_) for @stags;
+		printf "%s\n", join $opt_d, @vals if @vals;
 	}
 }
 
@@ -73,19 +87,37 @@ sub get_tag_values {
 
 	# then object members
 	if ($feature->can($tag)) {
-		my $val = $feature->$tag;
-		return $val->seq if ref($val) eq 'Bio::PrimarySeq';
-		return $val;
+		my @vals = $feature->$tag;
+		return $vals[0]->seq if ref($vals[0]) eq 'Bio::PrimarySeq';
+		return join ";", @vals;
 	}
 
 	# and reserved tags
-	if ($tag eq 'translate') {
-		return translate_feature($feature);
-	}
 	if ($tag eq 'join') {
 		return join_feature($feature);
 	}
+	return '';
+}
 
+# get value for a given seq/tag pair
+sub get_seq_values {
+	my $seq = shift;
+	my $tag = shift;
+	my %replace = (
+		locus      => 'display_id',
+		definition => 'desc',
+		accession  => 'accession_number',
+		organism   => 'species',
+		source     => 'species'
+	);
+	$tag = $replace{$tag} if defined $replace{$tag};
+	if ($seq->can($tag)) {
+		my @vals = $seq->$tag;
+		my $ref = ref($vals[0]);
+		return $vals[0]->seq      if $ref eq 'Bio::Seq::RichSeq';
+		return $vals[0]->binomial if $ref eq 'Bio::Species';
+		return join ";", @vals;
+	}
 	return '';
 }
 
@@ -93,18 +125,20 @@ sub get_tag_values {
 sub join_feature {
 	my $feature = shift;
 	my $loc = $feature->location;
-	return sprintf "%s..%s", $loc->start, $loc->end if not $loc->isa('Bio::Location::SplitLocationI');
+	return location_range($loc) if not $loc->isa('Bio::Location::SplitLocationI');
 	my @f = ();
-	push @f, sprintf "%s..%s", $_->start, $_->end for ($loc->sub_Location);
+	push @f, location_range($_) for ($loc->sub_Location);
 	return join ",", @f;
 }
 
-# translate the feature anew
-sub translate_feature {
-	my $feature = shift;
-	my $seq = $feature->spliced_seq;
-	my $table = get_tag_values($feature, 'transl_table');
-	return $seq->translate(-codontable_id => $table)->seq if $table;
-	return '';
+sub location_range {
+	my $loc = shift;
+	return sprintf "%s%s..%s%s", pos_type($loc->start_pos_type), $loc->start, pos_type($loc->end_pos_type), $loc->end;
 }
 
+sub pos_type {
+	my $pos = shift;
+	my %vals = ( BEFORE => '<', AFTER => '>', EXACT => '' ); # WITHIN => '', BETWEEN => ''
+	return $vals{$pos} if defined $vals{$pos};
+	return '';
+}
